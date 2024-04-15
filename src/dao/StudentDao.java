a85217dfb80e79ed94ae7848bfc5054ba9f08dbb
oohara <oohara@172.16.31.82>
2024-04-15 12:46:37 +0900

    2024/04/15

--- src/dao/StudentDao.java
+++ src/dao/StudentDao.java
@@ -241,38 +241,112 @@ public class StudentDao extends Dao {
 			statement.setString(1, no);
 			//プリペアードステートメントを実行
 			ResultSet rSet = statement.executeQuery();
+
+			//学校Daoを初期化
+			SchoolDao schoolDao = new SchoolDao();
+
+			if (rSet.next()) {
+				//リザルトセットが存在する場合
+				//学生インスタンスに検索結果をセット
+				student.setNo(rSet.getString("no"));
+				student.setName(rSet.getString("name"));
+				student.setEntYear(rSet.getInt("ent_year"));
+				student.setClassNum(rSet.getString("class_num"));
+				student.setAttend(rSet.getBoolean("is_attend"));
+				//学校フィールドには学校コードで検索した学校インスタンスをセット
+				student.setSchool(schoolDao.get(rSet.getString("school_cd")));
+			} else {
+				//リザルトセットが存在しない場合
+				//学生インスタンスにnullをセット
+				student = null;
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			//プリペアードステートメントを閉じる
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			//コネクションを閉じる
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+
+		return student;
+	}
+	public boolean save(Student student) throws Exception {
+		//コネクションを確立
+		Connection connection = getConnection();
+		//プリペアードステートメント
+		PreparedStatement statement = null;
+		//実行件数
+		int count = 0;
+
+		try {
+			//データベースから学生を取得
+			Student old = get(student.getNo());
+			if (old == null) {
+				//学生が存在しなかった場合
+				//プリペアードステートメントにINSERT文をセット
+				statement = connection.prepareStatement("insert into student(no, name, ent_year, class_num, is_attend, school_cd) values(?, ?, ?, ?, ?, ?)");
+				//プリペアードステートメントに値をバインド
+				statement.setString(1, student.getNo());
+				statement.setString(2, student.getName());
+				statement.setInt(3, student.getEntYear());
+				statement.setString(4, student.getClassNum());
+				statement.setBoolean(5, student.isAttend());
+				statement.setString(6, student.getSchool().getCd());
+			} else {
+				//学生が存在した場合
+				//プリペアードステートメントにUPDATE文をセット
+				statement = connection.prepareStatement("update student set name=?, ent_year=?, class_num=?, is_attend=? where no=?");
+				//プリペアードステートメントに値をバインド
+				statement.setString(1, student.getName());
+				statement.setInt(2,  student.getEntYear());
+				statement.setString(3,  student.getClassNum());
+				statement.setBoolean(4,  student.isAttend());
+				statement.setString(5, student.getNo());
+			}
+
+			//プリペアードステートメントを実行
+			count = statement.executeUpdate();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			//プリペアードステートメントを閉じる
+			if (statement != null) {
+				try {
+					statement.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+			//コネクションを閉じる
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException sqle) {
+					throw sqle;
+				}
+			}
+		}
+
+		if (count > 0) {
+			//実行件数が1件以上ある場合
+			return true;
+		} else {
+			//実行件数が0件の場合
+			return false;
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
